32f5a2396d096785a24c357923d7876a40e674bc
www <wolfs.axelw@gmail.com>
2018-07-10 10:19:26 +0200

    Creation Tableau

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,15 @@
+//Déclaration de variable
 var maVille;
 var monQuatier;
 var maConstance;
+
+/*//Création d'un module
+import {Personnage} from '../partials/_main.js'
+var personnage = new Personnage*/
+
+//Creation tableau
+var tab = ["hazard","de Bruyne","lukaku","courtois","meunier","origi","mitchy","kompany","vertonghen","axel oui moi x)","marouan","chaldi","youri"]
+
+tab.splice(0,"","Hazard","De Bruyne","Lukaku","Courtois","Meunier","Origi","Mitchy","Kompany","Vertonghen","Axel oui moi x)","Marouan","Chaldi","Youri")
+console.log(tab)
 
---
+++ public/partials/_main.js
@@ -0,0 +1,11 @@
+export class Personnage{
+  constructor(name, age, sex, height, weight){
+  this.name= name;
+  this.age=age;
+  this.sex=sex;
+  this.height=height;
+  this.weight=weight;
+}
+}
+
+const gravite
